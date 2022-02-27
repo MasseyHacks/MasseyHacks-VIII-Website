a9a9ca8643cfb126237e801ae24730387f6b0e0a
Eston <liyuelin2004@gmail.com>
2022-02-26 20:10:28 -0500

    Completed Startup Section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Navbar Functions
 const navLinksNode = document.querySelector(".nav-links");
 const navBurgerNode = document.querySelector(".nav-burger");
 const navNode = document.querySelector("nav");
@@ -6,4 +7,83 @@ const toggleNavLinks = () => {
     navLinksNode.classList.toggle("nav-links-active");
     navBurgerNode.classList.toggle("nav-burger-active");
     navNode.classList.toggle("nav-active");
+};
+
+//Startup Folder Functions
+const weekTwoImgNode = document.querySelector("#week-2-img");
+const weekThreeImgNode = document.querySelector("#week-3-img");
+const weekFiveImgNode = document.querySelector("#week-5-img");
+
+const openTab = (tabID) => {
+    switch (tabID) {
+        case 1:
+            weekTwoImgNode.classList.add("d-none");
+            weekThreeImgNode.classList.add("d-none");
+            weekFiveImgNode.classList.add("d-none");
+            break;
+        case 2:
+            weekTwoImgNode.classList.remove("d-none");
+            weekThreeImgNode.classList.add("d-none");
+            weekFiveImgNode.classList.add("d-none");
+            break;
+        case 3:
+            weekTwoImgNode.classList.add("d-none");
+            weekThreeImgNode.classList.remove("d-none");
+            weekFiveImgNode.classList.add("d-none");
+            break;
+        case 4:
+            weekTwoImgNode.classList.add("d-none");
+            weekThreeImgNode.classList.add("d-none");
+            weekFiveImgNode.classList.remove("d-none");
+            break;
+    }
+};
+
+const initFolderBtn = () => {
+    const weekOneBtnNode = document.querySelector(".startup-folder-buttons > span:nth-child(1)");
+    const weekTwoBtnNode = document.querySelector(".startup-folder-buttons > span:nth-child(2)");
+    const weekThreeBtnNode = document.querySelector(".startup-folder-buttons > span:nth-child(3)");
+    const weekFiveBtnNode = document.querySelector(".startup-folder-buttons > span:nth-child(4)");
+
+    weekOneBtnNode.innerHTML = "WEEK 1";
+    weekTwoBtnNode.innerHTML = "WEEK 2";
+    weekThreeBtnNode.innerHTML = "WEEK 3/4";
+    weekFiveBtnNode.innerHTML = "WEEK 5";
+
+
+    weekOneBtnNode.addEventListener("click", () => {
+        openTab(1);
+    });
+
+    weekTwoBtnNode.addEventListener("click", () => {
+        openTab(2);
+    });
+
+    weekThreeBtnNode.addEventListener("click", () => {
+        openTab(3);
+    });
+
+    weekFiveBtnNode.addEventListener("click", () => {
+        openTab(4);
+    });
+};
+
+const swiper = new Swiper('.folder-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+swiper.on('slideChange', function () {
+    openTab(swiper.realIndex + 1);
+});
+
+window.onload = () => {
+    initFolderBtn();
 };
