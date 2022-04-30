ca350a4cdcbbc67a8eaba683f42c9e66b426ed6e
Eston Li <liyuelin2004@gmail.com>
2022-04-30 19:58:31 -0400

    Transfer to Laptop

--- js/schedule.js
+++ js/schedule.js
@@ -22,12 +22,16 @@ class TimeLineGrid {
         }
     }
 
-    setTimeCell(col, row, title, description) {
+    addFirstCell(row, col, startGap, duration, title, description, colour) {
         const lineNode = document.createElement("div");
-        lineNode.style.width = "100%";
+        lineNode.style.width = `${(duration - Math.floor(duration)) * 100}%`;
         lineNode.style.height = "5px";
-        lineNode.style.backgroundColor = "purple";
+        lineNode.style.backgroundColor = colour;
         lineNode.style.position = "relative";
+
+        if (startGap - Math.floor(startGap) > 0.001) {
+            lineNode.style.left = `${(startGap - Math.floor(startGap)) * 100}%`;
+        }
 
         const textSection = document.createElement("div");
         textSection.className = "timeline-text-section";
@@ -43,6 +47,30 @@ class TimeLineGrid {
         lineNode.appendChild(textSection);
 
         this.timeCellNodes[col-1][row-1].appendChild(lineNode);
+    }
+
+    setTimeCell(row, col, startGap, duration, title, description, colour) {
+        this.addFirstCell(row, col, startGap, duration, title, description, colour);
+
+        let time = 1;
+        while (time < Math.floor(duration)) {
+            const lineNode = document.createElement("div");
+            lineNode.style.width = "100%";
+            lineNode.style.height = "5px";
+            lineNode.style.backgroundColor = colour;
+            lineNode.style.position = "relative";
+            this.timeCellNodes[col-1 + time][row-1].appendChild(lineNode);
+            time++;
+        }
+
+        if (duration - Math.floor(duration) > 0.001 && time < Math.floor(duration)) {
+            const lineNode = document.createElement("div");
+            lineNode.style.width = `${(duration - Math.floor(duration)) * 100}%`;
+            lineNode.style.height = "5px";
+            lineNode.style.backgroundColor = colour;
+            lineNode.style.position = "relative";
+            this.timeCellNodes[col-1 + time][row-1].appendChild(lineNode);
+        }
     }
 
     generateLabel() {
@@ -66,21 +94,13 @@ class TimeLineGrid {
     }
 }
 
-class TimeCell {
-    title = "";
-    description = "";
-    fromTime = null;
-    toTime = null;
-
-    constructor (title, description, fromTime, toTime) {
-        this.title = title;
-        this.description = description;
-        this.fromTime = fromTime;
-        this.toTime = toTime;
-    }
+const initTimeLine = (timeLine) => {
+    timeLine.setTimeCell(1, 1, 0.5, 0.75, "Check In", "9:00AM - 9:45AM | Front Desk", "purple");
+    timeLine.setTimeCell(1, 3, 0, 0.75, "Check In", "9:00AM - 9:45AM | Front Desk", "purple");
 }
 
 window.onload = () => {
     const timeLine = new TimeLineGrid();
-    timeLine.setTimeCell(5, 3, "Hello", "Gamer Ree");
+    initTimeLine(timeLine);
 };
+
